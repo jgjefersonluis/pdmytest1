3bb5c0b293c6c5a4ad79664921a31228ba8d61e6
Jeferson Luis <jgjefersonluis@gmail.com>
2021-06-28 03:35:37 -0300

    finalizado

--- pdmytest1/settings.py
+++ pdmytest1/settings.py
@@ -115,15 +115,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGOUT_REDIRECT_URL = 'index'
